f4b88176e93baa6aa54de6a8aa229bbd0c7bd079
xArch9x <bendt03@outlook.co.nz>
2023-06-15 08:31:05 +1200

    Changed ordering system

--- CS-103_Team_Assignment/CS-103_Team_Assignment.cpp
+++ CS-103_Team_Assignment/CS-103_Team_Assignment.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 void welcomeMessage()
 {
-	cout << "**********************" << endl;
-	cout << "**********************" << endl;
-	cout << "**** SCHOOL LUNCH ****" << endl;
-	cout << "**********************" << endl;
-	cout << "**********************\n" << endl;
+	cout << "************************" << endl;
+	cout << "************************" << endl;
+	cout << "***** SCHOOL LUNCH *****" << endl;
+	cout << "************************" << endl;
+	cout << "************************\n" << endl;
 
 
 }
@@ -17,31 +17,66 @@ void foodMenu()
 {
 	cout << "\n 1. PIE              -  $3.00" << endl;
 	cout << " 2. HAMBURGER        -  $3.50" << endl;
-	cout << " 3. NACHOS           -  $4.50" << endl;
-	cout << " 4. ONIGIRI          -  $2.00" << endl;
-	cout << " 5. TOFU ROLL        -  $3.25" << endl;
-	cout << " 6. SAUSAGE ROLL     -  $3.00" << endl;
-	cout << " 7. CHICKEN WRAP     -  $5.00" << endl;
-	cout << " 8. CHOCOLATE MUFFIN -  $2.50" << endl;
-	cout << " 9. BROWNIE          -  $2.00" << endl;
-	cout << "10. DRINKS           -  $2.00" << endl;
+	cout << " 3. SAUSAGE ROLL     -  $3.00" << endl;
+	cout << " 4. CHICKEN WRAP     -  $5.00" << endl;
+	cout << " 5. CHOCOLATE MUFFIN -  $2.50" << endl;
+	cout << " 6. BROWNIE          -  $2.00" << endl;
+	cout << " 7. DRINK            -  $2.00" << endl;
 }
 
 
 void drinkMenu()
 {
+	cout << "ALL DRINKS $2.00" << endl;
 	cout << "1. COKE" << endl;
 	cout << "2. ORANGE JUICE" << endl;
 	cout << "3. APPLE JUICE" << endl;
 	cout << "4. SPRITE" << endl;
 	cout << "5. L&P" << endl;
+	cout << "6. NONE" << endl;
 }
 
 void deals()
 {
 	cout << "\nCURRENT DEALS:" << endl;
 	cout << "BUY A PIE & DRINK, GET BROWNIE FOR FREE!!!" << endl;
-	cout << "BUY NACHOS GET DRINK HALF PRICE!!!" << endl;
+	cout << "BUY A CHICKEN WRAP GET DRINK HALF PRICE!!!" << endl;
+}
+
+void orderFood()
+{
+	cout << "ORDER PLACEHOLDER" << endl;
+}
+
+double calculateBill(int choice, int quantity) {
+	double price;
+	switch (choice) {
+	case 1:
+		price = 3.00;
+		break;
+	case 2:
+		price = 3.50;
+		break;
+	case 3:
+		price = 3.00;
+		break;
+	case 4: 
+		price = 5.00;
+		break;
+	case 5:
+		price = 2.50;
+		break;
+	case 6:
+		price = 2.00;
+		break;
+	case 7:
+		price = 2.00;
+		break;
+	default:
+		price = 0.00;
+		break;
+
+	}
 }
 
 
@@ -49,6 +84,9 @@ int main()
 {
 	welcomeMessage();
 
+	int choice, quantity;
+	double totalBill;
+	char paymentMethod;
 	char menuChoice;
 
 	cout << "1. Food Menu" << endl;
@@ -80,16 +118,23 @@ int main()
 	switch (menuChoice)
 	{
 	case '1':
-		deals();
+		cout << "******************************" << endl;
+		cout << "FOOD";
 		foodMenu();
-		break;
-
+		cout << "******************************" << endl;
+		cout << "DRINKS";
+		drinkMenu();
+		cout << "******************************" << endl;
+	
 	case '2':
-		break;
+		cout << "Enter your choice for food: ";
+		cin >> choice;
+		cout << "Enter quantity: ";
+		cin >> quantity;
 
-	case '3':
-		cout << "GOOD BYE" << endl;
-		return 0;
+		totalBill = calculateBill(choice, quantity);
+
+		cout << "Total Bill: " << endl;
 
 	}
 }
