d3f4679b3dc5e34544bf8d85556435c11f0dcc5f
Ben Du Toit <129692623+xArch9x@users.noreply.github.com>
2023-06-23 09:10:24 +1200

    Ordering (#11)
    * Added order
    
    * Ordering system updated
    
    * fixy
    
    * Ordering & Payment be done
    
    * fixed
    
    ---------
    
    Co-authored-by: Hudson Curren <hudson.curren@icloud.com>

--- CS-103_Team_Assignment/Common.h
+++ CS-103_Team_Assignment/Common.h
@@ -231,14 +231,6 @@ namespace utils {
 	static inline int gen_random_int(int min = 0, int max = INT32_MAX) {
 		return (rand() % max) + min;
 	}
-	// src: https://stackoverflow.com/questions/236129/how-do-i-iterate-over-the-words-of-a-string
-	static inline void split(const std::string& s, char delim, std::vector<std::string>* result) {
-		std::istringstream iss(s);
-		std::string item;
-		while (std::getline(iss, item, delim)) {
-			result->push_back(item);
-		}
-	}
 }
 
 #endif
